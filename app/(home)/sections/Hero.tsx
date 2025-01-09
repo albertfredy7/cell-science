a9b4654e1ecdbe077b1d2531ff03d2d8ab20e0e0
albertfredy7 <albertfredy007@gmail.com>
2025-01-09 20:04:37 +0530

    chore: add VSCode configuration for recommended extensions and update settings

--- app/(home)/sections/Hero.tsx
+++ app/(home)/sections/Hero.tsx
@@ -1,7 +1,6 @@
 import { CustomButton } from '@/components/CustomButton';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import React from 'react';
 
 const Hero = () => {
     return (
